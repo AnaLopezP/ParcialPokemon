476b56cd59cbe955a3df9966f5c05dd68e11832f
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-04-08 11:45:16 +0200

    funciones getter y setter

--- Pokemon/funciones.py
+++ Pokemon/funciones.py
@@ -9,4 +9,39 @@ class Pokemon:
         self.pv = pv
         self.pa = pa
         self.pd = pd
+
+    def get_id(self):
+        return self.id
+   
+    def set_id(self, id):
+        self.id = id
+    
+    def get_nombre(self):
+        return self.nombre
+   
+    def set_nombre(self, nombre):
+        self.nombre = nombre
         
+    def get_arma(self):
+        return self.arma
+
+    def set_arma(self, arma):
+        self.arma = arma
+        
+    def get_pv(self):
+        return self.pv
+   
+    def set_pv(self, pv):
+        self.pv= pv
+        
+    def get_pa(self):
+        return self.pa
+   
+    def set_pa(self, pa):
+        self.pa= pa
+        
+    def get_pd(self):
+        return self.pd
+   
+    def set_pd(self, pd):
+        self.pd= pd
