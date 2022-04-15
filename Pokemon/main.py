6ed2acfa6638013fbe7d3ec14e3dabc0bed6b111
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-04-15 12:50:57 +0200

    j

--- Pokemon/main.py
+++ Pokemon/main.py
@@ -2,9 +2,9 @@ import funciones
 
 if __name__ == '__main__':
     Eevee = funciones.Pokemon(1234, "Eevee", "Cabezazo", 59, 8, 4)
-    print(Eevee.cadena())
+    print(Eevee.__str__())
     Charmander = funciones.Pokemon(2345, "Charmander", "Puñetazo", 76, 5, 6)
-    print(Charmander.cadena())
+    print(Charmander.__str__())
     funciones.Pokemon.defensa(Charmander, 5)
     funciones.Pokemon.ataque(Eevee, Charmander)
     
